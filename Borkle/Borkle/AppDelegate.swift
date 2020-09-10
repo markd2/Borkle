ba4a063c32b514819b5db8a44d2438de3db5729a
Mark Dalrymple <markd@bignerdranch.com>
2020-09-10 18:00:50 -0400

    New Mac app from Xcode(11) template

---
+++ Borkle/Borkle/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Borkle
+//
+//  Created by markd on 9/10/20.
+//  Copyright © 2020 Borkware. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
