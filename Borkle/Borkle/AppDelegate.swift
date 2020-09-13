a80a29da1ed345943631982718cc2a305e045da9
markd <markd@borkware.com>
2020-09-13 11:02:52 -0400

    Reopen last opened file on launch.

--- Borkle/Borkle/AppDelegate.swift
+++ Borkle/Borkle/AppDelegate.swift
@@ -21,6 +21,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-
+    func applicationShouldOpenUntitledFile(_ sender: NSApplication) -> Bool {
+        
+        let documentController = NSDocumentController.shared
+        
+        if let mostRecentDocument = documentController.recentDocumentURLs.first {
+            documentController.openDocument(withContentsOf: mostRecentDocument, 
+                display: true, 
+                completionHandler: { (document, documentWasAlreadyOpen, errorWhileOpening) in
+                    Swift.print("Error restoring document \(String(describing: errorWhileOpening))")
+                })
+            return false
+        } else {
+            return true
+        }
+    }
 }
 
