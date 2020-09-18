090702edbb9633568858fc96ebf3426e07257373
markd <markd@borkware.com>
2020-09-18 17:12:44 -0400

    Adds "add bubbles to soup"

--- Borkle/Borkle/BubbleSoup.swift
+++ Borkle/Borkle/BubbleSoup.swift
@@ -1,1 +1,14 @@
 import Foundation
+
+class BubbleSoup {
+    private var bubbles: [Bubble] = []
+
+    func add(bubbles: [Bubble]) {
+        self.bubbles += bubbles
+    }
+
+    var bubbleCount: Int {
+        return bubbles.count
+    }
+    
+}
--- Borkle/BorkleTests/BubbleSoupTests.swift
+++ Borkle/BorkleTests/BubbleSoupTests.swift
@@ -8,8 +8,35 @@ class BubbleSoupTests: XCTestCase {
     override func tearDown() {
     }
 
-    func test_all_the_things() {
+    func test_add_bulk_bubbles_to_empty_soup() {
+        let soup = BubbleSoup()
+        let bubbles = [Bubble(ID: 1)]
+
+        soup.add(bubbles: bubbles)
+        XCTAssertEqual(soup.bubbleCount, 1)
     }
+
+    func test_add_bulk_bubbles_to_soup() {
+        let soup = BubbleSoup()
+        let bubbles = [Bubble(ID: 1)]
+
+        soup.add(bubbles: bubbles)
+        XCTAssertEqual(soup.bubbleCount, 1)
+    }
+
+    func test_adding_bulk_bubbles_undo() {
+        let soup = BubbleSoup()
+
+        let bubbles = [Bubble(ID: 1)]
+        soup.add(bubbles: bubbles)
+
+        let moreBubbles = [Bubble(ID: 2), Bubble(ID: 3)]
+        soup.add(bubbles: moreBubbles)
+
+        XCTAssertEqual(soup.bubbleCount, 3)
+    }
+
+ 
 }
 
 
