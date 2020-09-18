a9e85c382a1acfcee09b6fca12d157599d4aa5e1
markd <markd@borkware.com>
2020-09-18 17:01:22 -0400

    Scaffolding in tests.

--- Borkle/BorkleTests/BubbleSoupTests.swift
+++ Borkle/BorkleTests/BubbleSoupTests.swift
@@ -1,1 +1,15 @@
-import Foundation
+import XCTest
+@testable import Borkle
+
+class BubbleSoupTests: XCTestCase {
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+    func test_all_the_things() {
+    }
+}
+
+
