3bf39097ace6e7664e3283010c8fc5b010ca328c
markd <markd@borkware.com>
2020-09-18 21:43:52 -0400

    Adds removal, and undo tests (plus fallout in the soup)

--- Borkle/Borkle/BubbleSoup.swift
+++ Borkle/Borkle/BubbleSoup.swift
@@ -1,14 +1,44 @@
 import Foundation
 
+
 class BubbleSoup {
     private var bubbles: [Bubble] = []
-
-    func add(bubbles: [Bubble]) {
-        self.bubbles += bubbles
+    private let undoManager: UndoManager
+    
+    public init(undoManager: UndoManager? = nil) {
+        self.undoManager = undoManager ?? UndoManager()
+        undoManager?.groupsByEvent = false
     }
 
-    var bubbleCount: Int {
+    public func add(bubbles: [Bubble]) {
+        add(bubblesArray: bubbles)
+    }
+
+    internal func add(bubblesArray bubbles: [Bubble]) {
+        undoManager.beginUndoGrouping()
+        self.bubbles += bubbles
+        undoManager.registerUndo(withTarget: self) { selfTarget in
+            self.removeLastBubbles(count: bubbles.count)
+        }
+        undoManager.endUndoGrouping()
+    }
+
+    internal func removeLastBubbles(count: Int) {
+        undoManager.beginUndoGrouping()
+        let lastChunk = Array(self.bubbles.suffix(count))
+        bubbles.removeLast(count)
+        undoManager.registerUndo(withTarget: self) { selfTarget in
+            self.add(bubbles: lastChunk)
+        }
+        undoManager.endUndoGrouping()
+    }
+
+    public var bubbleCount: Int {
         return bubbles.count
+    }
+
+    internal func undo() {
+        undoManager.undoNestedGroup()
     }
     
 }
--- Borkle/BorkleTests/BubbleSoupTests.swift
+++ Borkle/BorkleTests/BubbleSoupTests.swift
@@ -18,9 +18,23 @@ class BubbleSoupTests: XCTestCase {
 
     func test_add_bulk_bubbles_to_soup() {
         let soup = BubbleSoup()
-        let bubbles = [Bubble(ID: 1)]
 
+        let bubbles = [Bubble(ID: 1)]
         soup.add(bubbles: bubbles)
+
+        let moreBubbles = [Bubble(ID: 2), Bubble(ID: 3)]
+        soup.add(bubbles: moreBubbles)
+
+        XCTAssertEqual(soup.bubbleCount, 3)
+    }
+
+    func test_remove_last_bubbles() {
+        let soup = BubbleSoup()
+
+        let bubbles = [Bubble(ID: 1), Bubble(ID: 2), Bubble(ID: 3)]
+        soup.add(bubbles: bubbles)
+
+        soup.removeLastBubbles(count: 2)
         XCTAssertEqual(soup.bubbleCount, 1)
     }
 
@@ -34,9 +48,14 @@ class BubbleSoupTests: XCTestCase {
         soup.add(bubbles: moreBubbles)
 
         XCTAssertEqual(soup.bubbleCount, 3)
+        soup.undo()
+        print("AFTER UNDO")
+        XCTAssertEqual(soup.bubbleCount, 1)
+        soup.undo()
+        XCTAssertEqual(soup.bubbleCount, 0)
+        print("AFTER UNDO")
     }
 
- 
 }
 
 
