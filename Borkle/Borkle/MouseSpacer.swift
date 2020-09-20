bec1b839399456d32d5e861368f12b91e2151816
markd <markd@borkware.com>
2020-09-20 19:42:34 -0400

    Adds Mouse Spacer starting point

---
+++ Borkle/Borkle/MouseSpacer.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+/// Mouse handler for clicks that start in space (blank canvas)
+
+class MouseSpacer {
+
+}
---
+++ Borkle/BorkleTests/MouseSpacerTests.swift
@@ -0,0 +1,17 @@
+import XCTest
+@testable import Borkle
+
+class MouseSpaceTests: XCTestCase {
+    var mouser: MouseSpacer!
+    
+    var invalCount = 0
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
