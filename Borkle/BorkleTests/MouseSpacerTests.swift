b8c36191a8547c74a2494fac2dc428db4e1b3f00
markd <markd@borkware.com>
2020-09-20 21:47:57 -0400

    Adds utility MouseSupport for tests.

--- Borkle/BorkleTests/MouseSpacerTests.swift
+++ Borkle/BorkleTests/MouseSpacerTests.swift
@@ -14,4 +14,34 @@ class MouseSpaceTests: XCTestCase {
         super.tearDown()
     }
 
+private class TestSupport: MouseSupport {
+
+    var hitTestBubbleArgument: CGPoint? = nil
+    var hitTestBubbleReturn: Bubble? = nil
+    func hitTestBubble(at: CGPoint) -> Bubble? {
+        hitTestBubbleArgument = at
+        return hitTestBubbleReturn
+    }
+
+    var areaTestBubblesArgument: CGRect? = nil
+    var areaTestBubblesReturn: [Bubble]? = nil
+    func areaTestBubbles(intersecting: CGRect) -> [Bubble]? {
+        areaTestBubblesArgument = intersecting
+        return areaTestBubblesReturn
+    }
+
+    var drawMarqueeArgument: CGRect? = nil
+    func drawMarquee(around: CGRect) {
+        drawMarqueeArgument = around
+    }
+
+    var unselectAllCalled = false
+    func unselectAll() {
+        unselectAllCalled = true
+    }
+
+    var selectArgument: [Bubble]? = nil
+    func select(bubbles: [Bubble]) {
+        selectArgument = bubbles
+    }
 }
