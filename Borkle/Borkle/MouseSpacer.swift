f83140d9aeeaef15e938d5b321b6dea08d9f845d
markd <markd@borkware.com>
2020-09-20 21:48:14 -0400

    Fixes "just a click deselects everything"

--- Borkle/Borkle/MouseSpacer.swift
+++ Borkle/Borkle/MouseSpacer.swift
@@ -26,13 +26,14 @@ class MouseSpacer: MouseHandler {
     }
 
     public func start(at point: CGPoint) {
+        // this will need to go somewhere else when we support shift-dragging
+        support.unselectAll()
         anchorPoint = point
     }
 
     public func move(to point: CGPoint) {
         let rect = CGRect(point1: point, point2: anchorPoint)
         support.drawMarquee(around: rect)
-        support.unselectAll()
 
         let bubbles = support.areaTestBubbles(intersecting: rect)
         if let bubbles = bubbles {
