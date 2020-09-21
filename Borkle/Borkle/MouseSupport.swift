f1d043e24f7d7752e6da0ebd2eacfd271aee2425
markd <markd@borkware.com>
2020-09-21 12:42:43 -0400

    Extracts mouse protocols to their own file.

---
+++ Borkle/Borkle/MouseSupport.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+protocol MouseHandler {
+    func start(at: CGPoint)
+    func move(to: CGPoint)
+    func finish()
+}
+
+protocol MouseSupport {
+    func hitTestBubble(at: CGPoint) -> Bubble?
+    func areaTestBubbles(intersecting: CGRect) -> [Bubble]?
+    func drawMarquee(around: CGRect)
+
+    func unselectAll()
+    func select(bubbles: [Bubble])
+}
