221c73eff56edaa192181538f1f6474f9d991e51
Mark Dalrymple <markd@borkware.com>
2020-09-27 19:37:13 -0400

    Adds mouse bubbler tests.

--- Borkle/BorkleTests/MouseBubblerTests.swift
+++ Borkle/BorkleTests/MouseBubblerTests.swift
@@ -3,18 +3,112 @@ import XCTest
 
 class MouseBubblerTests: XCTestCase {
     var mouser: MouseBubbler!
-    private var testSupport: TestSupport!
+    var testSupport: BubblerTestSupport!
+    var selection: Selection! = Selection()
+    var bubbles: [Bubble]!
 
     override func setUp() {
         super.setUp()
-        testSupport = TestSupport()
-        mouser = MouseBubbler(withSupport: testSupport, selectedBubbles: Selection())
+        testSupport = BubblerTestSupport()
+        selection = Selection()
+        mouser = MouseBubbler(withSupport: testSupport, selectedBubbles: selection)
+        bubbles = [Bubble(ID: 11, position: CGPoint(x: 11, y: 11), width: 11),
+                   Bubble(ID: 33, position: CGPoint(x: 33, y: 33), width: 33),
+                   Bubble(ID: 22, position: CGPoint(x: 220, y: 220), width: 200),
+                   Bubble(ID: 1, position: CGPoint(x: 100, y: 100), width: 42)]
     }
 
     override func tearDown() {
-        testSupport = nil
         mouser = nil
+        testSupport = nil
+        selection = nil
+        bubbles = nil
         super.tearDown()
     }
+    
+    func test_window_coordinates() {
+        XCTAssertFalse(mouser.prefersWindowCoordinates)
+    }
 
+    func test_click_in_bubble_unselects_others() {
+        selection.select(bubbles: bubbles)
+        selection.deselect(bubble: bubbles[3])
+
+        testSupport.hitTestBubbleReturn = bubbles[3]
+        mouser.start(at: .zero, modifierFlags: [])
+        mouser.finish(modifierFlags: [])
+
+        XCTAssertEqual(selection.bubbleCount, 1)
+        XCTAssertEqual(selection.selectedBubbles, [bubbles[3]])
+    }
+
+    func test_shift_click_adds_to_selection() {
+        selection.select(bubbles: bubbles)
+        selection.deselect(bubble: bubbles[3])
+
+        testSupport.hitTestBubbleReturn = bubbles[3]
+        mouser.start(at: .zero, modifierFlags: [.shift])
+        mouser.finish(modifierFlags: [.shift])
+
+        XCTAssertEqual(selection.bubbleCount, 4)
+        XCTAssertEqual(Set(selection.selectedBubbles), Set(bubbles))
+    }
+    
+    func test_command_click_toggles_selected() {
+        selection.select(bubble: bubbles[1])
+        selection.select(bubble: bubbles[2])
+
+        testSupport.hitTestBubbleReturn = bubbles[2]
+        mouser.start(at: .zero, modifierFlags: [.command])
+        mouser.finish(modifierFlags: [.command])
+
+        XCTAssertEqual(selection.bubbleCount, 1)
+        XCTAssertEqual(selection.selectedBubbles, [bubbles[1]])
+    }
+
+    func test_drag_in_selection_moves_it() {
+        selection.select(bubble: bubbles[1])
+        selection.select(bubble: bubbles[2])
+
+        testSupport.hitTestBubbleReturn = bubbles[1]
+
+        let p10_20 = CGPoint(x: 10, y: 20)
+        let p20_40 = CGPoint(x: 20, y: 40)
+
+        mouser.start(at: .zero, modifierFlags: [])
+        mouser.drag(to: p10_20, modifierFlags: [])
+        mouser.drag(to: p20_40, modifierFlags: [])
+        
+        XCTAssertEqual(testSupport.moveAccumulator, 
+                       [BubblePoint(bubbles[1], CGPoint(x: 43, y: 53)),
+                        BubblePoint(bubbles[2], CGPoint(x: 230, y: 240)),
+                        BubblePoint(bubbles[1], CGPoint(x: 53, y: 73)),
+                        BubblePoint(bubbles[2], CGPoint(x: 240, y: 260))])
+        mouser.finish(modifierFlags: [])
+    }
+
+    func test_click_in_unselected_bubble_and_drag_unselects_first_then_drag() {
+    }
+}
+
+// Can't Equatable tuples :-(
+struct BubblePoint: Equatable {
+    let bubble: Bubble
+    let point: CGPoint
+    
+    init(_ bubble: Bubble, _ point: CGPoint) {
+        self.bubble = bubble
+        self.point = point
+    }
+}
+
+
+class BubblerTestSupport: TestSupport {
+    
+    var moveAccumulator: [BubblePoint]! = []
+    override func move(bubble: Bubble, to: CGPoint) {
+        print("SNORGLE \(bubble.ID) to \(to)")
+        moveAccumulator.append(BubblePoint(bubble, to))
+    }
+    
 }
