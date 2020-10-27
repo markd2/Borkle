1560efdbffe7a7ea2826255ff78f881a992f244a
Mark Dalrymple <markd@borkware.com>
2020-10-26 22:03:20 -0400

    Finishes implementation of RGB and colors.

--- Borkle/Borkle/Bubble.swift
+++ Borkle/Borkle/Bubble.swift
@@ -23,15 +23,39 @@ class Bubble: Codable {
         let red: CGFloat
         let green: CGFloat
         let blue: CGFloat
+
+        init(string: String?) {
+            guard let string = string else {
+                // no string, be obnoxious green
+                red = 0.0; green = 1.0; blue = 0.0
+                return
+            }
+
+            let chunks = string
+              .split(separator: " ")
+              .compactMap { String($0) }
+              .compactMap { CGFloat($0) }
+
+            guard chunks.count >= 3 else {
+                // not enough chunkage, be obnoxious green
+                red = 0.0; green = 1.0; blue = 0.0
+                return
+            }
+            red = chunks[0]
+            green = chunks[1]
+            blue = chunks[2]
+        }
     }
 
     var fillColorRGB: RGB?
     var fillColor: NSColor? {
-        return .red
+        guard let rgb = fillColorRGB else { return nil }
+        return NSColor.colorFromRGB(rgb)
     }
     var borderColorRGB: RGB?
     var borderColor: NSColor? {
-        return .blue
+        guard let rgb = borderColorRGB else { return nil }
+        return NSColor.colorFromRGB(rgb)
     }
     var borderThickness: Int?
 
@@ -224,6 +248,7 @@ extension Bubble: Hashable {
 }
 
 extension Bubble {
+
     func heightForStringDrawing() -> CGFloat {
         let textStorage = NSTextStorage.init(string: text, attributes: nil)
         let insetWidth = width - (Bubble.margin * 2)
@@ -243,3 +268,8 @@ extension Bubble {
     }
 }
 
+extension NSColor {
+    static func colorFromRGB(_ rgb: Bubble.RGB) -> NSColor {
+        NSColor.init(deviceRed: rgb.red, green: rgb.green, blue: rgb.blue, alpha: 1.0)
+    }
+}
