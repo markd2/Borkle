5a61e93bbc68053b469e37b45b1e589c1e85ff15
MarkD <markd@borkware.com>
2024-01-27 21:06:46 -0500

    adds connection tests

--- Borkle/BorkleTests/PlayfieldTests.swift
+++ Borkle/BorkleTests/PlayfieldTests.swift
@@ -27,6 +27,7 @@ final class PlayfieldTests: XCTestCase {
         for text in bubbleTexts {
             let bubble = soup.createNewBubble()
             bubble.text = text
+            playfield.addBubble(bubble)
             print(bubble)
         }
     }
@@ -34,11 +35,44 @@ final class PlayfieldTests: XCTestCase {
     // ----------
     // Tests
 
-    func testBubbleByID() throws {
+    func testBubbleByID() {
         let expectedBubble = playfield.bubble(byID: 1)
         XCTAssertNotNil(expectedBubble)
 
         let unexpectedBubble = playfield.bubble(byID: 0)
         XCTAssertNil(unexpectedBubble)
+    }
+
+    func testConnections() {
+        // nothing should be connected
+        for id in playfield.bubbleIdentifiers {
+            let connectedIndexes = playfield.connectionsForBubble(id: id)
+            XCTAssertEqual(connectedIndexes, IndexSet())
+        }
+
+        // connect two bubbles
+        playfield.addConnectionsBetween(bubbleIDs: [2], to: 1)
+        XCTAssertTrue(playfield.isBubble(1, connectedTo: 2))
+        XCTAssertTrue(playfield.isBubble(2, connectedTo: 1))
+
+        // make sure extras not connected
+        for id1 in playfield.bubbleIdentifiers {
+            for id2 in playfield.bubbleIdentifiers {
+                if (id1 == 1 && id2 == 2) || (id1 == 2 && id2 == 1) {
+                    continue
+                }
+                XCTAssertFalse(playfield.isBubble(id1, connectedTo: id2))
+                XCTAssertFalse(playfield.isBubble(id2, connectedTo: id1))
+            }
+        }
+
+        // disconnect them
+        playfield.disconnect(bubbleIDs: [1], from: 2)
+
+        // nothing should be connected now
+        for id in playfield.bubbleIdentifiers {
+            let connectedIndexes = playfield.connectionsForBubble(id: id)
+            XCTAssertEqual(connectedIndexes, IndexSet())
+        }
     }
 }
