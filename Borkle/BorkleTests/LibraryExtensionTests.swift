f0e051f0882864055e68a1d2f192cb43ca553606
Mark Dalrymple <markd@borkware.com>
2024-11-09 15:45:31 -0500

    Adds cgfloat-from-string test

--- Borkle/BorkleTests/LibraryExtensionTests.swift
+++ Borkle/BorkleTests/LibraryExtensionTests.swift
@@ -1,10 +1,28 @@
 import Testing
+import AppKit
 
 struct LibraryExtensionTests {
 
-    @Test func stringExtensions() {
+    @Test func stringTrimming() {
         let string = "    Stuff in the Middle      "
         #expect(string.trimmed == "Stuff in the Middle")
     }
-
+    
+    @Test(arguments: [
+            ("1.0", 1.0),
+            ("0.0", 0.0),
+            ("-0.0", -0.0),
+            ("-0.0", 0.0),
+            ("0.00000", 0.0),
+            ("3.02", 3.02),
+            ("-.23", -0.23),
+            ("2837.5e-2", 28.375),
+            ("0x1c.6", 28.375),
+            ("3.7796484325e+2", 377.96484325) // from an actual document
+          ])
+    func cgfloatFromString(_ string: String, expected: CGFloat) throws {
+        print(string, expected)
+        let value = try #require(CGFloat(string))
+        #expect(value.approxEqual(expected))
+    }
 }
